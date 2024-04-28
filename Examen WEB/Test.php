781ada7f7d960a95bd30255a4e1803922849f49d
AudreyR31 <audreyrivet@orange.fr>
2024-04-28 18:36:26 +0200

    Mise en ligne POrtfolio

---
+++ Examen WEB/Test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Capsules Vidéo</title>
+</head>
+
+<body>
+<h2>"Bonjour"</h2>
+<?php include("menu.php");?>
+</body>
+</html>
